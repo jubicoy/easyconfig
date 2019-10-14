bbb02e55ae0b5d3eada96055e43349129d9bc3b3
Vilppu Vuorinen <vilppu.vuorinen@jubic.fi>
2019-10-14 17:45:42 +0300

    Improved exception messages on parsing failures
    Exception message now contains name of the failing parameter.

--- easyconfig-core/src/main/java/fi/jubic/easyconfig/MappingException.java
+++ easyconfig-core/src/main/java/fi/jubic/easyconfig/MappingException.java
@@ -1,10 +1,23 @@
 package fi.jubic.easyconfig;
 
 public class MappingException extends Exception {
-    MappingException(String message) {
+    public MappingException() {
+        super();
+    }
+
+    public MappingException(String message) {
         super(message);
     }
-    public MappingException(Throwable throwable) {
-        super(throwable);
+
+    public MappingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public MappingException(Throwable cause) {
+        super(cause);
+    }
+
+    protected MappingException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
     }
 }
