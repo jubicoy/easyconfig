fc3d31cda7034df09adf88b0e62194319f1ac39b
Vilppu Vuorinen <8420095+vilppuvuorinen@users.noreply.github.com>
2023-12-12 22:38:53 +0200

    Bump slf4j to 2.0.7 to match newer logback
    - Update logger initialization in easyconfig-logback to work with
      1.4.14.
    
    - Use a mock appender instead of the XML configured TestAppender.

--- easyconfig-jdbc/src/test/java/fi/jubic/easyconfig/jdbc/DuplicateConfigWarningTest.java
+++ easyconfig-jdbc/src/test/java/fi/jubic/easyconfig/jdbc/DuplicateConfigWarningTest.java
@@ -1,38 +1,57 @@
 package fi.jubic.easyconfig.jdbc;
 
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
 import fi.jubic.easyconfig.ConfigMapper;
 import fi.jubic.easyconfig.providers.EnvProvider;
 import fi.jubic.easyconfig.providers.StaticEnvProvider;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.slf4j.event.Level;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 public class DuplicateConfigWarningTest {
     @Test
     void warnAboutDuplicateConfigs() {
-        TestAppender.clear();
+        var logger = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger(
+                Logger.ROOT_LOGGER_NAME
+        );
+        //noinspection unchecked
+        var appender = (Appender<ILoggingEvent>) mock(Appender.class);
+
+        logger.addAppender(appender);
 
         new ConfigMapper(envProvider1)
                 .read(PooledJdbcConfiguration.class);
         new ConfigMapper(envProvider2)
                 .read(PooledJdbcConfiguration.class);
 
-        assertEquals(
-                0,
-                TestAppender.events.size()
-        );
+        verify(appender, never()).doAppend(any());
 
         new ConfigMapper(envProvider1)
                 .read(PooledJdbcConfiguration.class);
 
-        assertEquals(
-                1,
-                TestAppender.events.size()
-        );
+        var captor = ArgumentCaptor.forClass(ILoggingEvent.class);
+
+        verify(appender, times(1)).doAppend(captor.capture());
+
         assertEquals(
                 Level.WARN.toString(),
-                TestAppender.events.get(0).getLevel().toString()
+                captor.getValue().getLevel().toString()
+        );
+        assertEquals(
+                "Multiple connection pools initialized with the same connection "
+                        + "parameters to jdbc:h2:./target/tmp/dup-test-1. Make sure a singleton "
+                        + "configuration is used.",
+                captor.getValue().getFormattedMessage()
         );
     }
 
